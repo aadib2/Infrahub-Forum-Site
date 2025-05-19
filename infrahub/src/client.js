7396a65f41efdc61b6f7bfb5e93cc84df8e3b8de
Aadi Bery <aadibery@gmail.com>
2025-05-19 01:16:46 -0700

    env file

--- infrahub/src/client.js
+++ infrahub/src/client.js
@@ -1,6 +1,6 @@
 import {createClient} from '@supabase/supabase-js'
 
-const URL = 'https://fexupfvmcpejodsdkdqg.supabase.co'
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZleHVwZnZtY3Blam9kc2RrZHFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0NTMxODYsImV4cCI6MjA2MTAyOTE4Nn0.ltabSxzzHIl7FZ-Fi-4dkoeocgI-TLiln5fVM2s1_jg'
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const API_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(URL, API_KEY);
